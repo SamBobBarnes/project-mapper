1dbe4b6042b66a1fc3d8a74151a20d6921a71e0d
SamBobBarnes <sbarnes@vermeer.com>
2025-09-16 10:52:18 -0500

    render to html

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import * as child_process from "node:child_process";
 
 const projectPath = process.argv[2];
+const flags = process.argv.slice(3);
 
 if (!projectPath) {
     console.error('Please provide a project path as an argument.');
@@ -22,8 +23,10 @@ if (!packageJson.dependencies) {
     process.exit(1);
 }
 
-console.log('Installing dependencies...');
-child_process.execSync('npm i', { cwd: projectPath });
+if(!flags.includes('--skip-install')){
+    console.log('Installing dependencies...');
+    child_process.execSync('npm i', { cwd: projectPath });
+}
 
 const dependencies = packageJson.dependencies;
 const devDependencies = packageJson.devDependencies || {};
@@ -52,27 +55,83 @@ const buildDependencyTree = (depName, visited = new Set()) => {
 
 const dependencyTree = Object.keys(dependencies).map(dep => buildDependencyTree(dep));
 
-console.log(JSON.stringify(dependencyTree, null, 2));
+// console.log(JSON.stringify(dependencyTree, null, 2));
 
+const packages = {};
+const reversePackagesMap = {};
 
-//
-// const writeToMermaid = (depTree) => {
-//     let mermaidStr = 'flowchart TD\n';
-//     const edges = new Set();
-//
-//     const traverse = (node) => {
-//         if (node.children) {
-//             node.children.forEach(child => {
-//                 edges.add(`    ["${node.name}"] --> ["${child.name}"]`);
-//                 traverse(child);
-//             });
-//         }
-//     };
-//     depTree.forEach(traverse);
-//     mermaidStr += Array.from(edges).join('\n');
-//     return mermaidStr;
-// };
-//
-// const mermaidOutput = writeToMermaid(dependencyTree);
-// fs.writeFileSync(`${projectPath}/dependency-tree.mmd`, mermaidOutput);
-// console.log(`Mermaid diagram written to ${projectPath}/dependency-tree.mmd`);
+let nextId = 1001;
+
+const buildUniqueIds = (node) => {
+    node.forEach(dep => {
+        const key = `${dep.name}@${dep.version}`;
+        if (!packages[key]) {
+            packages[key] = nextId;
+            reversePackagesMap[nextId] = key;
+            nextId++;
+        }
+        if (dep.children && dep.children.length > 0) {
+            buildUniqueIds(dep.children);
+        }
+    });
+}
+
+dependencyTree.forEach(dep => {
+    buildUniqueIds([dep]);
+})
+
+// Object.keys(packages).forEach((key)=>{
+//     console.log(`${packages[key]}: ${key}`);
+// })
+
+const writeToMermaid = (depTree) => {
+    let mermaidStr = 'flowchart TD\n';
+    const edges = new Set();
+
+    const traverse = (node) => {
+        if (node.children) {
+            node.children.forEach(child => {
+                edges.add(`    ${packages[node.name + '@' + node.version]}["${node.name}@${node.version}"] --> ${packages[child.name + '@' + child.version]}["${child.name}: ${child.version}"]`);
+                traverse(child);
+            });
+        }
+    };
+    depTree.forEach(traverse);
+    mermaidStr += Array.from(edges).join('\n');
+    return mermaidStr;
+};
+
+const mermaidOutput = writeToMermaid(dependencyTree);
+
+//write to html file
+const htmlContent = `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Large Mermaid Diagram</title>
+    </head>
+    <body>
+
+        <pre class="mermaid">
+            ${mermaidOutput}
+        </pre>
+        <script type="module">
+            import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs';
+            mermaid.initialize({
+                startOnLoad: true,
+                theme: 'default',
+                flowchart: {
+                    maxEdges: 2000 
+                },
+                maxTextSize: 100000,
+            });
+        </script>
+    </body>
+</html>
+`;
+
+fs.writeFileSync('dependency-graph.html', htmlContent);
+
+console.log('Dependency graph written to dependency-graph.html');
+console.log('Open this file in a web browser to view the graph.');
